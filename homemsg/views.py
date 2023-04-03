c0c4fd3a8eedb0bd24158667ad59910215d2de2a
liuzihaohao <liuzihaohao@users.noreply.github.com>
2023-04-03 21:04:07 +0800

    Signed-off-by: liuzihaohao <liuzihaohao@users.noreply.github.com>

--- homemsg/views.py
+++ homemsg/views.py
@@ -6,7 +6,8 @@ import markdown
 
 def getpagelist(request):
 	obj=models.articlePags.objects.all()
-	return render(request,'../template/homepage.html',{'obj':obj,'murl':"/media/"})
+	return render(request,'../template/homepagev2.html',{'obj':obj,'murl':"/media/"})
+
 def getpages(request,ida):
     obj=None
     try:
@@ -14,18 +15,6 @@ def getpages(request,ida):
     except Exception:
         return render(request,'../template/404.html')
     article=obj.mdtext
-    # article=markdown.markdown(article,extensions=[
-	# 	'markdown.extensions.extra',
-	# 	'markdown.extensions.codehilite',
-	# 	'markdown.extensions.toc',
-    #     'mdx_math',
-	# ],
-    # extension_configs={
-    #     'mdx_math': {
-    #         'enable_dollar_delimiter': True,  # 是否启用单美元符号（默认只启用双美元）
-    #         'add_preview': True  # 在公式加载成功前是否启用预览（默认不启用）
-    #     }
-    # })
     return render(request,'../template/articlepage.html', {'article': article,"title":obj.title,'time':obj.time})
 def getpagesida(request,ida):
     obj=None
@@ -34,16 +23,4 @@ def getpagesida(request,ida):
     except Exception:
         return render(request,'../template/404.html')
     article=obj.mdtext
-    # article=markdown.markdown(article,extensions=[
-	# 	'markdown.extensions.extra',
-	# 	'markdown.extensions.codehilite',
-	# 	'markdown.extensions.toc',
-    #     'mdx_math',
-	# ],
-    # extension_configs={
-    #     'mdx_math': {
-    #         'enable_dollar_delimiter': True,  # 是否启用单美元符号（默认只启用双美元）
-    #         'add_preview': True  # 在公式加载成功前是否启用预览（默认不启用）
-    #     }
-    # })
     return render(request,'../template/articlepage.html', {'article': article,"title":obj.title,'time':obj.time})
